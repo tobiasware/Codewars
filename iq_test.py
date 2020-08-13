d303de75eb544ad811b6ee6902df4bdd8aaf2263
tobiasware <tobias@mitterreiter.de>
2020-08-13 10:26:13 +0200

    complete

---
+++ iq_test.py
@@ -0,0 +1,16 @@
+def iq_test(numbers):
+	l = [int(x) % 2 for x in numbers.split(" ")]
+
+	if l.count(0) > 1:
+		print(l.index(0)+1)
+		return l.index(0)+1
+	else:
+		print(l.index(1)+1)
+		return l.index(1)+1
+
+
+
+
+
+iq_test("2 4 7 8 10") #3
+iq_test("1 2 2") #1
