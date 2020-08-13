5ba422a920abbdf06ca56330954b57785174123b
tobiasware <tobias@mitterreiter.de>
2020-08-13 10:37:34 +0200

    bug fix

--- iq_test.py
+++ iq_test.py
@@ -2,13 +2,13 @@ def iq_test(numbers):
 	l = [int(x) % 2 for x in numbers.split(" ")]
 
 	if l.count(0) > 1:
-		return l.index(0)+1
-	else:
 		return l.index(1)+1
+	else:
+		return l.index(0)+1
 
 
 
 
 
-iq_test("2 4 7 8 10") #3
-iq_test("1 2 2") #1
+print(iq_test("2 4 7 8 10")) #3
+print(iq_test("1 2 2")) #1
